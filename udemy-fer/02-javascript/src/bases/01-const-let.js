1346439ab03e25b6eafd8a0d6a63ffdb14663311
Alex Ballera <alexballera@gmail.com>
2020-06-29 12:56:19 -0300

    Let y const

---
+++ udemy-fer/02-javascript/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+// Variables y Constantes
+
+const nombre = 'Alexander'
+const apellido = 'Ballera'
+
+let valorDado = 5
+valorDado = 4
+
+console.log(nombre, apellido, valorDado)
+
+// No usar var!!!
+
+if (true) {
+  let valorDado = 6
+
+  console.log('valorDado', valorDado)
+}
