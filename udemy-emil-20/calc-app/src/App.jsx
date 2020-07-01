9900666276cb05aa88136816b5777e688c1190da
Alex Ballera <alexballera@gmail.com>
2020-07-01 16:08:09 -0300

    Agregado estilos

--- udemy-emil-20/calc-app/src/App.jsx
+++ udemy-emil-20/calc-app/src/App.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import './App.scss'
 
 const App = () => {
-  return <h1>Calc App</h1>
+  return (
+    <main className="react-calculator">
+      <div className="result">
+      </div>
+      <div className="numbers">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>10</button>
+      </div>
+      <div className="functions">
+        <button>
+          Clear
+        </button>
+        <button>
+          Remove
+        </button>
+      </div>
+
+      <div className="math-operations">
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>=</button>
+      </div>
+    </main>
+  )
 }
 
 export default App
